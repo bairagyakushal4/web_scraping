2279adfe8fc00557b07923e93096ddc19a6b22e0
Kushal Bairagya <59840475+bairagyakushal4@users.noreply.github.com>
2023-06-28 22:18:34 +0530

    Update convert_docx_to_epub.py
    new move function

--- dd_web_scraping/convert_docx_to_epub.py
+++ dd_web_scraping/convert_docx_to_epub.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 # install calibre
 # pip install capybre
 from capybre import convert
@@ -6,10 +7,20 @@ from capybre import convert
 
 allDoc = [file for file in os.listdir() if file.endswith(".docx")]
 
+if allDoc == []:
+    exit()
+
+
+os.mkdir('epub')
+
 for d in allDoc:
     FileFullName = os.path.basename(d)
     # doc_split = os.path.splitext()
-    # doc_only_name = doc_split[0]
+    # doc_only_name = doc_split[0]    
 
-    convert(FileFullName, as_ext='epub')
-    print(FileFullName)
+    newFile = convert(FileFullName, as_ext='epub')
+    new_path = 'epub/' + newFile
+        
+    shutil.move(newFile, new_path)
+    
+    print(new_path)
